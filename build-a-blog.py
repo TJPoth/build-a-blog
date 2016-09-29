b0d56bcf56619042ecff576dcc40ad65579a1dcd
TJPoth <tjpothier17@gmail.com>
2016-09-29 14:46:46 -0500

    Completed needed features

--- build-a-blog.py
+++ build-a-blog.py
@@ -11,22 +11,21 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
-
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class bloglist(db.Model):
+class BlogPosts(db.Model):
     title = db.StringProperty(required=True)
     body = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
 
 class HomePage(Handler):
-    def render_front(self, title="", body="", error=""):
-        self.render("blog.html", title=title, body=body, error=error)
+    def render_front(self):
+        blogs = db.GqlQuery("SELECT * FROM BlogPosts ORDER BY created DESC LIMIT 5")
+        self.render("home.html", blogs=blogs)
     def get(self):
         self.render_front()
     def post(self):
@@ -34,9 +33,57 @@ class HomePage(Handler):
         body = self.request.get("body")
 
         if title and body:
-            b = bloglist(title = title, body = body)
+            b = BlogPosts(title = title, body = body)
             b.put()
+            self.redirect('/')
+        else:
+            error = "Please enter both a title and a body"
+            self.render_front(title, body, error)
 
-        self.redirect('/')
+class NewPost(Handler):
+    def render_front(self, title="", body="", error=""):
+        self.render("newpost.html", title=title, body=body, error=error)
+    def get(self):
+        self.render_front()
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
 
-app = webapp2.WSGIApplication([('/', HomePage)], debug=True)
+        if title and body:
+            post = BlogPosts(title = title, body = body)
+            post.put()
+            post_id = post.key().id()
+            post_id = str(post_id)
+            self.redirect('/blog/' + post_id)
+        else:
+            error = "Please enter both a title and a body"
+            self.render_front(title, body, error)
+
+class AllBlogs(Handler):
+    def get(self):
+        allblogs = db.GqlQuery("SELECT * FROM BlogPosts ORDER BY created DESC")
+        post_id = []
+        n = 0
+        for blog in allblogs:
+            post_id.append(blog.key().id())
+            post_id[n] = str(post_id[n])
+            n+=1
+        self.render("allblogs.html", post_id=post_id, allblogs=allblogs, blog=blog)
+
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        id_num = int(id)
+        if BlogPosts.get_by_id(id_num):
+            blog = BlogPosts.get_by_id(id_num)
+            self.render("single-blog.html", blog=blog)
+        else:
+            self.response.write("not found")
+
+
+
+app = webapp2.WSGIApplication([('/', HomePage),
+                               ('/newpost', NewPost),
+                               ('/all-blogs', AllBlogs),
+                  webapp2.Route('/blog/<id:\d+>', ViewPostHandler)],
+                              debug=True)
